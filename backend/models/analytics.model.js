cc1b191c790171b101ea1470df5ea283607210b3
Amanze Bruno <167206012+brunoamanzechinaenyeze@users.noreply.github.com>
2024-12-13 23:22:05 +0100

    Update analytics.model.js

--- backend/models/analytics.model.js
+++ backend/models/analytics.model.js
@@ -4,37 +4,30 @@ const analyticsSchema = new mongoose.Schema({
     totalProductsSold: {
         type: Number,
         default: 0,
-        description: 'Total number of products sold'
     },
     availableStock: {
         type: Number,
         default: 0,
-        description: 'Number of products currently in stock'
     },
     totalRevenue: {
         type: Number,
         default: 0,
-        description: 'Total revenue generated'
     },
     totalOrders: {
         type: Number,
         default: 0,
-        description: 'Total number of orders placed'
     },
     completedOrders: {
         type: Number,
         default: 0,
-        description: 'Total number of completed orders'
     },
     pageViews: {
         type: Number,
         default: 0,
-        description: 'Total number of page views'
     },
     createdAt: {
         type: Date,
         default: Date.now,
-        description: 'Timestamp when analytics data was recorded'
     }
 }, { timestamps: true });
 
